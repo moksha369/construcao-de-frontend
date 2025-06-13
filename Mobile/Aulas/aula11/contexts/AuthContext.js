c6b0d8df16c527b56d8f4fe5dee3a86f335a9972
unknown <2324291056@academico.iesb.br>
2025-06-13 20:03:26 -0300

    aula11

---
+++ Mobile/Aulas/aula11/contexts/AuthContext.js
@@ -0,0 +1,39 @@
+import { createContext, useState } from "react";
+import * as AuthService from "../services/AuthService";
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [usuario, setUsuario] = useState({ login: null, logado: false });
+  const [erro, setErro] = useState("");
+
+  const login = async ({email, senha}) => {
+    try {
+        await AuthService.signIn({email: email, password: senha, returnSecureToken: true});
+        setUsuario({login: email, logado: true});
+        setErro("");
+    } catch(error) {
+        setErro(`Deu ruim: ${error.message}`);
+    }
+  }
+
+  const logout = () => {
+    setUsuario({ login: null, logado: false });
+  }
+
+
+  const registrar = async ({email, senha}) => {
+    try {
+        await AuthService.signUp({email: email, password: senha, returnSecureToken: true});
+        setUsuario({login: email, logado: true});
+        setErro("");
+    } catch(error) {
+        setErro(`Deu ruim: ${error.message}`);
+    }
+  }
+
+  return (
+    <AuthContext.Provider value={{ usuario, erro, login, logout, registrar }}>{children}</AuthContext.Provider>
+  );
+};
+
+export { AuthContext, AuthProvider };
---
+++ Mobile/Aulas/aula11/services/AuthService.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+const API_URL = process.env.EXPO_PUBLIC_API_URL;
+const API_KEY = process.env.EXPO_PUBLIC_API_KEY;
+
+async function signIn(credenciais) {
+  try {
+    const response = await axios.post(
+      `${API_URL}:signInWithPassword?key=${API_KEY}`,
+      credenciais
+    );
+    return response.data;
+  } catch (error) {
+    const {
+      error: { message },
+    } = error.response.data;
+    if (message === "INVALID_LOGIN_CREDENTIALS") {
+      throw new Error("Usuario/Senha invalidos");
+    }
+    throw error;
+  }
+}
+
+async function signUp(credenciais) {
+  try {
+    const response = await axios.post(
+      `${API_URL}:signUp?key=${API_KEY}`,
+      credenciais
+    );
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export { signIn, signUp };
